a990cc9e702b07747b9417787e5068f87c7fdd74
Glangine Geoffrey <geoffrey.glangine@gmail.com>
2015-01-23 21:12:56 +0000

    finalisation du serveur temporaire

--- src/fr/kwidz/JeuDeDames/Serveur/ThreadConnexion.java
+++ src/fr/kwidz/JeuDeDames/Serveur/ThreadConnexion.java
@@ -1,16 +1,34 @@
 package fr.kwidz.JeuDeDames.Serveur;
 
+import java.io.*;
 import java.net.Socket;
 
 /**
  * Created by kwidz on 23/01/15.
  */
 public class ThreadConnexion extends Thread{
-    public ThreadConnexion(Socket socketClient) {
 
+    private BufferedReader input;
+    private PrintWriter output;
+    private Socket socketClient;
+
+    public ThreadConnexion(Socket socketClient) {
+        this.socketClient = socketClient;
+
+        // Association d'un flux d'entree et de sortie
+        try {
+            input = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream())), true);
+        } catch(IOException e) {
+            System.err.println("Association des flux impossible : " + e);
+            System.exit(-1);
+        }
     }
 
+
+    @Override
     public void run() {
 
+        System.out.println("test");
     }
 }
