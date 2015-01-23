6730d35533ae0eb595eb555c32309793d9dbe0e2
geoffrey.glangine <geoffrey.glangine@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-01-23 21:04:51 +0000

    creation d'une classe de thread et de l'écoute du serveur

--- trunk/src/fr/kwidz/JeuDeDames/Serveur/ServeurTCPMulti.java
+++ trunk/src/fr/kwidz/JeuDeDames/Serveur/ServeurTCPMulti.java
@@ -1,7 +1,46 @@
 package fr.kwidz.JeuDeDames.Serveur;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by kwidz on 23/01/15.
  */
 public class ServeurTCPMulti {
+    public static final int portEcoute = 56880;
+
+    public static void main(String[] args){
+        ServerSocket socketServeur = null;
+        try {
+            socketServeur = new ServerSocket(portEcoute);
+        } catch(IOException e) {
+            System.err.println("Creation de la socket impossible : " + e);
+            System.exit(-1);
+        }
+
+        // Attente des connexions des clients
+        try {
+            Socket socketClient;
+            while(true) {
+                socketClient = socketServeur.accept();
+                ThreadConnexion t = new ThreadConnexion(socketClient);
+                t.start();
+            }
+        } catch(IOException e) {
+            System.err.println("Erreur lors de l'attente d'une connexion : " + e);
+            System.exit(-1);
+        }
+
+        // Fermeture de la socket
+        try {
+            socketServeur.close();
+        } catch(IOException e) {
+            System.err.println("Erreur lors de la fermeture de la socket : " + e);
+            System.exit(-1);
+        }
+    }
+    }
+
+
 }
---
+++ trunk/src/fr/kwidz/JeuDeDames/Serveur/ThreadConnexion.java
@@ -0,0 +1,16 @@
+package fr.kwidz.JeuDeDames.Serveur;
+
+import java.net.Socket;
+
+/**
+ * Created by kwidz on 23/01/15.
+ */
+public class ThreadConnexion extends Thread{
+    public ThreadConnexion(Socket socketClient) {
+
+    }
+
+    public void run() {
+
+    }
+}
