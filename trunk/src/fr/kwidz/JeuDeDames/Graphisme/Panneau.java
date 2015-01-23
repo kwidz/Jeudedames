1d36c30dffc3e0f770668d02936debb27e0138be
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-01-23 21:48:07 +0000

    Ajout d'une image

--- trunk/src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
+++ trunk/src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
@@ -4,13 +4,26 @@ package fr.kwidz.JeuDeDames.Graphisme;
  * Created by morgane on 23/01/15.
  */
 
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Panneau extends JPanel{
-    public Panneau(){
+    public Panneau()  {
         this.setBackground(Color.red);
+        try {
+            this.ajoutPion();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-
+    public void ajoutPion() throws IOException {
+        BufferedImage myPicture = ImageIO.read(new File("Images/pion.png"));
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+        add(picLabel);
     }
 }
