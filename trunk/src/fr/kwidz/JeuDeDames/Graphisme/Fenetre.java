e15b8f0948194a46d7dc31fcb6b18b9ebb7def80
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-01-23 21:08:05 +0000

    Parametrage de la fenetre

--- trunk/src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
+++ trunk/src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
@@ -8,6 +8,11 @@ import javax.swing.JFrame;
 public class Fenetre {
         public  Fenetre(){
             JFrame maFenetre = new JFrame();
+
+            maFenetre.setTitle("Jeu de Dames");
+            maFenetre.setSize(400, 100);
+            maFenetre.setLocationRelativeTo(null);
+            maFenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             maFenetre.setVisible(true);
         }
 
