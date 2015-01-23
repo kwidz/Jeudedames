5bdbf423906f67c3c4960ddef4300ed76adec060
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-01-23 21:04:37 +0000

    Apparition d'une fenetre

--- trunk/src/fr/kwidz/JeuDeDames/Main.java
+++ trunk/src/fr/kwidz/JeuDeDames/Main.java
@@ -1,8 +1,11 @@
 package fr.kwidz.JeuDeDames;
 
+import fr.kwidz.JeuDeDames.Graphisme.Fenetre;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Fenetre f = new Fenetre();
     }
 }
