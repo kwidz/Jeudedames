6a69e5391754d9b1866ce27b5ef2a8e71699e3c5
morgane.cabrol <morganec@users.noreply.github.com>
2015-01-23 21:04:37 +0000

    Apparition d'une fenetre

--- src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
+++ src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
@@ -3,5 +3,14 @@ package fr.kwidz.JeuDeDames.Graphisme;
 /**
  * Created by morgane on 23/01/15.
  */
+
+import javax.swing.JFrame;
 public class Fenetre {
+        public  Fenetre(){
+            JFrame maFenetre = new JFrame();
+            maFenetre.setVisible(true);
+        }
+
+
+
 }
