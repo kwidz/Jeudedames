ccf334aa5c2111697ed2034ee668352a93a3cc4b
morgane.cabrol <morganec@users.noreply.github.com>
2015-01-27 14:47:12 +0000

    re-structuration

--- src/fr/kwidz/JeuDeDames/Jeu/Damier.java
+++ src/fr/kwidz/JeuDeDames/Jeu/Damier.java
@@ -1,7 +1,13 @@
 package fr.kwidz.JeuDeDames.Jeu;
 
+
+
 /**
  * Created by kwidz on 27/01/15.
  */
 public class Damier {
+    Case[][] damier = new Case[10][10]; //Le dammier contient un tableau de 10*10 cases
+    public Damier(){
+
+    }
 }
