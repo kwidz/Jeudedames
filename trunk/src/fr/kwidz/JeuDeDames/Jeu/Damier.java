da02a36fa6cb8d8a8979d67cc9a71f22150ca14b
geoffrey.glangine <geoffrey.glangine@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-01-27 15:02:36 +0000

    creation du tableau de cases

--- trunk/src/fr/kwidz/JeuDeDames/Jeu/Damier.java
+++ trunk/src/fr/kwidz/JeuDeDames/Jeu/Damier.java
@@ -6,7 +6,7 @@ package fr.kwidz.JeuDeDames.Jeu;
  * Created by kwidz on 27/01/15.
  */
 public class Damier {
-    Case[][] lesCases = new Case[10][10]; //Le dammier contient un tableau de 10*10 cases
+    public Case[][] lesCases = new Case[10][10]; //Le dammier contient un tableau de 10*10 cases
     public Damier(){
         boolean white=true;
         for (int i = 0; i < 10; i++) {
