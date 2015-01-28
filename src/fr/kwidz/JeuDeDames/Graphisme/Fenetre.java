dbdb361b6ee1142c0b705614e17e89c66e41bd2f
Glangine Geoffrey <geoffrey.glangine@gmail.com>
2015-01-28 16:18:12 +0000

    redimentionnement de la fennetre

--- src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
+++ src/fr/kwidz/JeuDeDames/Graphisme/Fenetre.java
@@ -10,20 +10,25 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
 public class Fenetre extends JFrame{
-    Panneau tableauJeu;
+    public Panneau tableauJeu;
+    public Fenetre self = this;
         public  Fenetre(Panneau p){
+
             this.addComponentListener(new ComponentAdapter()
             {
                 public void componentResized(ComponentEvent evt) {
-                    Component c = (Component)evt.getSource();
-                    //........
-                    System.out.print("resize");
+                    if(self.getHeight()>self.getWidth()) {
+                        self.setSize(self.getHeight(), self.getHeight());
+                    }
+                    else {
+                        self.setSize(self.getWidth(), self.getWidth());
+                    }
                 }
             });
 
             this.tableauJeu = p;
             this.setTitle("Jeu de Dames");
-            this.setSize(400, 400);
+            this.setSize(610, 630);
             this.setLocationRelativeTo(null);
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setContentPane(this.tableauJeu);
