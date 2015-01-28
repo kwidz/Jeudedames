24ac0c64dc4ac483440608ebc51f4ddedbc0fcec
Glangine Geoffrey <geoffrey.glangine@gmail.com>
2015-01-28 16:24:48 +0000

    redimentionnement de la fennetre

--- src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
+++ src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
@@ -6,17 +6,9 @@ package fr.kwidz.JeuDeDames.Graphisme;
 
 import fr.kwidz.JeuDeDames.Jeu.CaseNoire;
 import fr.kwidz.JeuDeDames.Jeu.Damier;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 
 public class Panneau extends JPanel{
     Damier damier;
