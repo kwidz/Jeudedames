47a032bc4f2ce4aa2fd9b640d83e30e604580135
morgane.cabrol <morganec@users.noreply.github.com>
2015-01-30 21:52:37 +0000

    ajout bouton dammier

--- src/fr/kwidz/JeuDeDames/Jeu/Case.java
+++ src/fr/kwidz/JeuDeDames/Jeu/Case.java
@@ -8,6 +8,6 @@ import fr.kwidz.JeuDeDames.Graphisme.Bouton;
 public class Case{
     public Bouton boutonContenu;
     public Case(){
-        //boutonContenu = new Bouton();
+        boutonContenu = new Bouton();
     }
 }
