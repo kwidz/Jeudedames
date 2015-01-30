41265146777faee88c395753ce96665147f66217
morgane.cabrol <morganec@users.noreply.github.com>
2015-01-30 21:03:50 +0000

    ajout bouton dammier

--- src/fr/kwidz/JeuDeDames/Jeu/Jeu.java
+++ src/fr/kwidz/JeuDeDames/Jeu/Jeu.java
@@ -12,7 +12,7 @@ public class Jeu {
     Damier damier;
     public Jeu(){
         this.damier = new Damier();
-        vueJeu = new Affichage();
+        vueJeu = new Affichage(damier);
         vueJeu.raffraichir(this.damier);
         //Panneau p = new Panneau();
         //Fenetre f = new Fenetre(p);
