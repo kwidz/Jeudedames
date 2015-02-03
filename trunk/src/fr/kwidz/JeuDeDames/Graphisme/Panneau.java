63511f425a3973669703c6574e79bb9074a84888
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-02-03 16:09:44 +0000

    affichafge pion

--- trunk/src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
+++ trunk/src/fr/kwidz/JeuDeDames/Graphisme/Panneau.java
@@ -111,7 +111,14 @@ public class Panneau extends JPanel{
                     if (this.damier.lesCases[i][j] instanceof CaseNoire) {
 
                         damier.lesCases[i][j].boutonContenu.modifierBouton(i * this.tailleFenetreW, j * this.tailleFenetreH, tailleFenetreW, tailleFenetreH);
-                        damier.lesCases[i][j].boutonContenu.dessinerPion(i , j , tailleFenetreW-10, tailleFenetreH-10, Color.blue);
+                        if(damier.lesCases[i][j].pion != null){
+                            if(damier.lesCases[i][j].pion.blanc){
+                                damier.lesCases[i][j].boutonContenu.dessinerPion(i , j , tailleFenetreW-10, tailleFenetreH-10, Color.white);
+                            }else{
+                                damier.lesCases[i][j].boutonContenu.dessinerPion(i , j , tailleFenetreW-10, tailleFenetreH-10, Color.red);
+                            }
+                        }
+
 
                     } else {
 
--- trunk/src/fr/kwidz/JeuDeDames/Jeu/Pion.java
+++ trunk/src/fr/kwidz/JeuDeDames/Jeu/Pion.java
@@ -4,7 +4,7 @@ package fr.kwidz.JeuDeDames.Jeu;
  * Created by kwidz on 27/01/15.
  */
 public class Pion extends Piece{
-    boolean blanc;
+    public boolean blanc;
     public Pion(boolean blanc){
         this.blanc = blanc;
     }
