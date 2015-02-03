0afa42ee0ca5db053bb2d3ad7c9667731695dd2a
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-02-03 15:56:31 +0000

    Affichage pion

--- trunk/src/fr/kwidz/JeuDeDames/Jeu/Case.java
+++ trunk/src/fr/kwidz/JeuDeDames/Jeu/Case.java
@@ -7,7 +7,7 @@ import fr.kwidz.JeuDeDames.Graphisme.Bouton;
  */
 public class Case{
     public Bouton boutonContenu;
-
+    public Pion pion;
     public Case(){
         boutonContenu = new Bouton();
     }
