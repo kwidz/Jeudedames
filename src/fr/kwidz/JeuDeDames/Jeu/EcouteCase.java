9032670a9d8e26af45dbcee337837a63a67e0b71
morgane.cabrol <morganec@users.noreply.github.com>
2015-02-06 20:54:28 +0000

    Deplacement pion

---
+++ src/fr/kwidz/JeuDeDames/Jeu/EcouteCase.java
@@ -0,0 +1,35 @@
+package fr.kwidz.JeuDeDames.Jeu;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Created by morgane on 06/02/15.
+ */
+public class EcouteCase implements MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
