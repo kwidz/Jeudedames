a34da14ac4c9d718b46a09868c0442c2c136de9a
morgane.cabrol <morganec@users.noreply.github.com>
2015-02-13 22:03:33 +0000

    ne marche plus qu'avec les rouges

--- src/fr/kwidz/JeuDeDames/Jeu/EcouteCase.java
+++ src/fr/kwidz/JeuDeDames/Jeu/EcouteCase.java
@@ -19,6 +19,7 @@ public class EcouteCase implements MouseListener{
     public Case caseDepart;
     public GestionaireDeTours jeton;
     public DialogueAvecServeur dialogue;
+    public Boolean joueur1;
     ArrayList chemin = new ArrayList();
 
     public  EcouteCase(Case unecase, Damier undamier, GestionaireDeTours jeton, DialogueAvecServeur dialogue){
@@ -27,21 +28,69 @@ public class EcouteCase implements MouseListener{
         this.contenuCase = unecase.caseDrawableContenu;
         this.dialogue=dialogue;
         this.jeton=jeton;
+        this.joueur1 = jeton.isJoueur1();
     }
     @Override
     public void mouseClicked(MouseEvent e) {
 
-        //chemin.addAll( this.leDamier.TrouverCheminPossible(this.contenuCase.caseX, this.contenuCase.caseY));
-        if(this.lacase.pion != null){
+
+        if(jeton.isJeton()){  // si c'est a son tour de jouer
+            if(this.lacase.pion != null){
+                if( (joueur1 && this.lacase.pion.blanc)  || (!joueur1 && (this.lacase.pion.blanc)==false) ){   //si il clique sur son le bon pion
+                    if(this.lacase.estSelectionne) {
+                        this.lacase.DeSelectionner();
+                        chemin = leDamier.getChemin();
+                        for (int i = 0; i < chemin.size(); i++) {
+                            Case c = (Case) chemin.get(i);
+                            c.choisissable = false;
+                            c.DeSelectionner();
+                        }
+                        leDamier.EffacerChemin();
+                    }else{
+                        leDamier.EffacerChemin();
+                        this.leDamier.TrouverCheminPossible(this.contenuCase.caseX, this.contenuCase.caseY);
+                        this.lacase.Selectionner();
+                        chemin = this.leDamier.getChemin();
+                        for(int i=1 ; i < chemin.size() ; i++){
+                            Case c = (Case)chemin.get(i);
+                            c.choisissable = true;
+                            c.Selectionner();
+                        }
+                    }
+
+
+
+
+                    }else{
+
+                }
+            }else{
+                if(this.lacase.estSelectionne && this.lacase.choisissable){
+                    jouerUnCoups();
+                }
+                this.lacase.DeSelectionner();
+                chemin = leDamier.getChemin();
+                for(int i=0 ; i < chemin.size() ; i++){
+                    Case c = (Case)chemin.get(i);
+                    c.choisissable = false;
+                    c.DeSelectionner();
+                }
+                leDamier.EffacerChemin();
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+/*        if(this.lacase.pion != null){
             System.out.print("test");
             if(this.lacase.estSelectionne){
-                /*if(this.lacase.choisissable) {
-                    Case caseDepart = (Case) this.leDamier.getChemin().get(0);
-                    Color c = caseDepart.caseDrawableContenu.couleurPion;
-                    caseDepart.EffacerPion();
-                    this.lacase.caseDrawableContenu.modifierCouleurPion(c);
-
-                }*/
                 this.lacase.DeSelectionner();
                 chemin = leDamier.getChemin();
                 for(int i=0 ; i < chemin.size() ; i++){
@@ -90,7 +139,7 @@ public class EcouteCase implements MouseListener{
                 c.DeSelectionner();
             }
             leDamier.EffacerChemin();
-        }
+        }*/
 
 
 
