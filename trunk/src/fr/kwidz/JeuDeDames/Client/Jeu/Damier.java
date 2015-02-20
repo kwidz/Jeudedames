85067ebdca927db5abbe2851567b0538cd1e5c5c
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-02-20 21:35:25 +0000

    Preparation refactorisation fonction trouver chemin

--- trunk/src/fr/kwidz/JeuDeDames/Client/Jeu/Damier.java
+++ trunk/src/fr/kwidz/JeuDeDames/Client/Jeu/Damier.java
@@ -56,36 +56,17 @@ public class Damier {
         //System.out.print("dammier"+blanc);
 
         if(blanc){
-            if(casex+1 < 10 && casey+1 < 10){
-                if(lesCases[casex+1][casey+1].pion == null){
-                    chemin.add(lesCases[casex+1][casey+1]);
-                }else{
 
-                    if(casex+2 < 10 && casey+2 < 10){
-                        if(lesCases[casex+2][casey+2].pion == null){
-                            chemin.add(lesCases[casex+2][casey+2]);
-                        }
-                    }
 
+
+
+            if(this.verification(casex,casey,1,1)){
+                chemin.add(lesCases[casex+1][casey+1]);
+            }else{
+                if(this.verification(casex,casey,2,2)){
+                    chemin.add(lesCases[casex+2][casey+2]);
                 }
             }
-
-/*
-
-            if(casex+1 < 10 && casey+1 < 10){
-                if(lesCases[casex+1][casey+1].pion != null){
-                    if(casex+2 < 10 && casey+2 < 10){
-                        if(lesCases[casex+2][casey+2].pion == null){
-                            chemin.add(lesCases[casex+2][casey+2]);
-                        }
-                    }
-                }else{
-                    chemin.add(lesCases[casex+1][casey+1]);
-                }
-            }
-*/
-
-
 
 
 
@@ -101,33 +82,27 @@ public class Damier {
         }else{
 
 
-            if(casex-1 > -1 && casey-1 > -1){
-                if(lesCases[casex-1][casey-1].pion == null){
-                    chemin.add(lesCases[casex-1][casey-1]);
-                }else{
 
-                    if(casex-2 > -1 && casey-2  > -1){
-                        if(lesCases[casex-2][casey-2].pion == null){
-                            chemin.add(lesCases[casex-2][casey-2]);
-                        }
-                    }
 
+
+            if(this.verification(casex,casey,-1,-1)){
+                chemin.add(lesCases[casex-1][casey-1]);
+            }else{
+                if(this.verification(casex,casey,-2,-2)){
+                    chemin.add(lesCases[casex-2][casey-2]);
                 }
             }
 
 
-            if(casex-1 > -1  && casey+1 < 10){
-                if(lesCases[casex-1][casey+1].pion == null){
-                    chemin.add(lesCases[casex-1][casey+1]);
-                }else{
-                    if(casex-2 > -1 && casey+2 < 10){
-                        if(lesCases[casex-2][casey+2].pion == null){
-                            chemin.add(lesCases[casex - 2][casey + 2]);
-                        }
-                    }
 
+            if(this.verification(casex,casey,-1,1)){
+                chemin.add(lesCases[casex-1][casey+1]);
+            }else{
+                if(this.verification(casex,casey,-2,2)){
+                    chemin.add(lesCases[casex-2][casey+2]);
                 }
             }
+
 
         }
 
