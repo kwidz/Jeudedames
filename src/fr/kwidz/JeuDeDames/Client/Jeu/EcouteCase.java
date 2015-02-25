6967a98227d161ded5407c6a84e55c28bad9b8ce
morgane.cabrol <morganec@users.noreply.github.com>
2015-02-25 15:59:01 +0000

    prise de pion

--- src/fr/kwidz/JeuDeDames/Client/Jeu/EcouteCase.java
+++ src/fr/kwidz/JeuDeDames/Client/Jeu/EcouteCase.java
@@ -3,6 +3,8 @@ package fr.kwidz.JeuDeDames.Client.Jeu;
 import fr.kwidz.JeuDeDames.Client.Graphisme.CaseDrawable;
 import fr.kwidz.JeuDeDames.Client.Reseau.DialogueAvecServeur;
 import fr.kwidz.JeuDeDames.Client.Reseau.GestionaireDeTours;
+
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -10,7 +12,7 @@ import java.util.ArrayList;
 /**
  * Created by morgane on 06/02/15.
  */
-public class EcouteCase implements MouseListener{
+public class EcouteCase implements MouseListener {
     public Case lacase;
     public Damier leDamier;
     public CaseDrawable contenuCase;
@@ -19,63 +21,66 @@ public class EcouteCase implements MouseListener{
     public DialogueAvecServeur dialogue;
     public Boolean joueur1;
     ArrayList chemin = new ArrayList();
+    ArrayList pionPris = new ArrayList();
 
-    public  EcouteCase(Case unecase, Damier undamier, GestionaireDeTours jeton, DialogueAvecServeur dialogue){
+    public EcouteCase(Case unecase, Damier undamier, GestionaireDeTours jeton, DialogueAvecServeur dialogue) {
         this.lacase = unecase;
         this.leDamier = undamier;
         this.contenuCase = unecase.caseDrawableContenu;
-        this.dialogue=dialogue;
-        this.jeton=jeton;
+        this.dialogue = dialogue;
+        this.jeton = jeton;
 
     }
+
     @Override
     public void mouseClicked(MouseEvent e) {
 
 
-System.out.println("dans ecoute case :" + this.jeton.isJoueur1());
+        System.out.println("dans ecoute case :" + this.jeton.isJoueur1());
 
 
-if(jeton.isJeton()){
-    if(this.peutJoueur()){
-        jouerUnCoups();
-        this.lacase.DeSelectionner();
-        chemin = leDamier.getChemin();
-        for(int i=0 ; i < chemin.size() ; i++){
-            Case c = (Case)chemin.get(i);
-            c.choisissable = false;
-            c.DeSelectionner();
+        if (jeton.isJeton()) {
+            if (this.peutJoueur()) {
+                this.pionPris();
+                jouerUnCoups();
+                this.lacase.DeSelectionner();
+                chemin = leDamier.getChemin();
+                for (int i = 0; i < chemin.size(); i++) {
+                    Case c = (Case) chemin.get(i);
+                    c.choisissable = false;
+                    c.DeSelectionner();
+                }
+                leDamier.EffacerChemin();
+            } else {
+                if (this.estSelectionnable()) {
+                    chemin = leDamier.getChemin();
+                    for (int i = 0; i < chemin.size(); i++) {
+                        Case c = (Case) chemin.get(i);
+                        c.choisissable = false;
+                        c.DeSelectionner();
+                    }
+                    leDamier.EffacerChemin();
+                    this.leDamier.TrouverCheminPossible(this.contenuCase.caseX, this.contenuCase.caseY, this.jeton.isJoueur1());
+                    this.lacase.Selectionner();
+                    chemin = this.leDamier.getChemin();
+                    for (int i = 1; i < chemin.size(); i++) {
+                        Case c = (Case) chemin.get(i);
+                        c.choisissable = true;
+                        c.Selectionner();
+                    }
+                } else {
+                    this.lacase.DeSelectionner();
+                    chemin = leDamier.getChemin();
+                    for (int i = 0; i < chemin.size(); i++) {
+                        Case c = (Case) chemin.get(i);
+                        c.choisissable = false;
+                        c.DeSelectionner();
+                    }
+                    leDamier.EffacerChemin();
+                }
+            }
+
         }
-        leDamier.EffacerChemin();
-    }else{
-        if(this.estSelectionnable()){
-            chemin = leDamier.getChemin();
-            for(int i=0 ; i < chemin.size() ; i++){
-                Case c = (Case)chemin.get(i);
-                c.choisissable = false;
-                c.DeSelectionner();
-            }
-            leDamier.EffacerChemin();
-            this.leDamier.TrouverCheminPossible(this.contenuCase.caseX, this.contenuCase.caseY, this.jeton.isJoueur1());
-            this.lacase.Selectionner();
-            chemin = this.leDamier.getChemin();
-            for(int i=1 ; i < chemin.size() ; i++){
-                Case c = (Case)chemin.get(i);
-                c.choisissable = true;
-                c.Selectionner();
-            }
-        }else{
-            this.lacase.DeSelectionner();
-            chemin = leDamier.getChemin();
-            for(int i=0 ; i < chemin.size() ; i++){
-                Case c = (Case)chemin.get(i);
-                c.choisissable = false;
-                c.DeSelectionner();
-            }
-            leDamier.EffacerChemin();
-        }
-    }
-
-}
 
 
     }
@@ -100,44 +105,50 @@ if(jeton.isJeton()){
 
     }
 
-    private void jouerUnCoups(){
-        if(jeton.isJoueur1()){
+    private void jouerUnCoups() {
+        if (jeton.isJoueur1()) {
             System.out.println("HERE IS JOUEUR1");
-        }else{
+        } else {
             System.out.println("HERE IS JOUEUR2");
         }
-        if(jeton.isJeton()) {
+        if (jeton.isJeton()) {
             Case caseDepart = (Case) this.leDamier.getChemin().get(0);
             this.lacase.pion = new Pion(caseDepart.pion.blanc);
             this.lacase.caseDrawableContenu.dessinerPion(caseDepart.caseDrawableContenu.pionPosX, caseDepart.caseDrawableContenu.pionPosY, caseDepart.caseDrawableContenu.pionWidth, caseDepart.caseDrawableContenu.pionHeight, caseDepart.caseDrawableContenu.couleurPion);
             caseDepart.EffacerPion();
+
+            for(int i = 0 ; i< pionPris.size() ; i++){
+                Point p = (Point)pionPris.get(i);
+                this.leDamier.lesCases[p.x][p.y].EffacerPion();
+            }
+
             String mouvement = new String("caseDepart=" + caseDepart.caseDrawableContenu.caseX + "," + caseDepart.caseDrawableContenu.caseY + ";caseArrivee=" + this.lacase.caseDrawableContenu.caseX + "," + this.lacase.caseDrawableContenu.caseY);
             System.out.println(mouvement);
-            dialogue.jouerUnCoups(caseDepart.caseDrawableContenu.caseX,caseDepart.caseDrawableContenu.caseY,this.lacase.caseDrawableContenu.caseX,this.lacase.caseDrawableContenu.caseY);
+            dialogue.jouerUnCoups(caseDepart.caseDrawableContenu.caseX, caseDepart.caseDrawableContenu.caseY, this.lacase.caseDrawableContenu.caseX, this.lacase.caseDrawableContenu.caseY);
             jeton.setJeton(false);
         }
     }
 
 
-    public boolean peutJoueur(){
-        if(this.lacase.estSelectionne && this.lacase.choisissable && (this.lacase.pion == null)){
+    public boolean peutJoueur() {
+        if (this.lacase.estSelectionne && this.lacase.choisissable && (this.lacase.pion == null)) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    public boolean estSelectionnable(){
-        if(lacase.pion != null){
-            if(jeton.isJoueur1() ){
-                if(lacase.pion.blanc){
-                    if(this.lacase.estSelectionne == false){
+    public boolean estSelectionnable() {
+        if (lacase.pion != null) {
+            if (jeton.isJoueur1()) {
+                if (lacase.pion.blanc) {
+                    if (this.lacase.estSelectionne == false) {
                         return true;
                     }
                 }
-            }else{
-                if(lacase.pion.blanc == false){
-                    if(this.lacase.estSelectionne == false){
+            } else {
+                if (lacase.pion.blanc == false) {
+                    if (this.lacase.estSelectionne == false) {
                         return true;
                     }
                 }
@@ -146,5 +157,28 @@ if(jeton.isJeton()){
         return false;
 
 
+    }
+
+    public void pionPris() {
+        Case caseDepart = (Case) this.leDamier.getChemin().get(0);
+        if(((caseDepart.caseDrawableContenu.caseX + 2)  == (this.lacase.caseDrawableContenu.caseX))  && ((caseDepart.caseDrawableContenu.caseY + 2)  == (this.lacase.caseDrawableContenu.caseY)) ){
+            Point p = new Point((caseDepart.caseDrawableContenu.caseX + 1),(caseDepart.caseDrawableContenu.caseY + 1));
+            this.pionPris.add(p);
+        }
+
+        if(((caseDepart.caseDrawableContenu.caseX + 2)  == (this.lacase.caseDrawableContenu.caseX))  && ((caseDepart.caseDrawableContenu.caseY - 2)  == (this.lacase.caseDrawableContenu.caseY)) ){
+            Point p = new Point((caseDepart.caseDrawableContenu.caseX + 1),(caseDepart.caseDrawableContenu.caseY - 1));
+            this.pionPris.add(p);
+        }
+
+        if(((caseDepart.caseDrawableContenu.caseX - 2)  == (this.lacase.caseDrawableContenu.caseX))  && ((caseDepart.caseDrawableContenu.caseY - 2)  == (this.lacase.caseDrawableContenu.caseY)) ){
+            Point p = new Point((caseDepart.caseDrawableContenu.caseX - 1),(caseDepart.caseDrawableContenu.caseY - 1));
+            this.pionPris.add(p);
+        }
+
+        if(((caseDepart.caseDrawableContenu.caseX - 2)  == (this.lacase.caseDrawableContenu.caseX))  && ((caseDepart.caseDrawableContenu.caseY + 2)  == (this.lacase.caseDrawableContenu.caseY)) ){
+            Point p = new Point((caseDepart.caseDrawableContenu.caseX - 1),(caseDepart.caseDrawableContenu.caseY + 1));
+            this.pionPris.add(p);
+        }
     }
 }
