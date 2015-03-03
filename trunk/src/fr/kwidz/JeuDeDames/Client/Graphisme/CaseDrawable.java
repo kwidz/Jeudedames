7378ced8fea0e841e3d7b38c01d9bfda50553458
morgane.cabrol <morgane.cabrol@8e9b31f1-49eb-4578-a787-5167d0c6818f>
2015-03-03 15:33:51 +0000

    Deplacement dame

--- trunk/src/fr/kwidz/JeuDeDames/Client/Graphisme/CaseDrawable.java
+++ trunk/src/fr/kwidz/JeuDeDames/Client/Graphisme/CaseDrawable.java
@@ -62,7 +62,7 @@ public class CaseDrawable extends JPanel{
             }else{
                 //Dessiner dame
                 g.setColor(Color.BLUE);
-                g.drawString("DAME", this.pionPosX+6, this.pionPosY);
+                g.drawString("DAME", this.pionPosX, this.pionPosY+50);
             }
         }else{
             g.dispose();
--- trunk/src/fr/kwidz/JeuDeDames/Client/Jeu/Damier.java
+++ trunk/src/fr/kwidz/JeuDeDames/Client/Jeu/Damier.java
@@ -43,7 +43,7 @@ public class Damier {
         }
     }
 
-    public void TrouverCheminPossible(int casex, int casey, boolean blanc){  //pour l'instant cette methode renvois une case aleatoire
+    public void TrouverCheminPossible(int casex, int casey, boolean blanc , Piece pieceCase){  //pour l'instant cette methode renvois une case aleatoire
 
       /*  chemin.add(lesCases[casex][casey]);
         chemin.add(lesCases[casex+1][casey+1]);
@@ -56,77 +56,91 @@ public class Damier {
         //System.out.print("dammier"+blanc);
 
         if(blanc){
-
-
-
-
-            if(this.verification(casex,casey,1,1,blanc)==1){
-                chemin.add(lesCases[casex+1][casey+1]);
-            }else if(this.verification(casex,casey,1,1,blanc)==2){
-                if(this.verification(casex,casey,2,2,blanc)==1){
-                    chemin.add(lesCases[casex+2][casey+2]);
+            if(pieceCase instanceof Pion){
+                if(this.verification(casex,casey,1,1,blanc)==1){
+                    chemin.add(lesCases[casex+1][casey+1]);
+                }else if(this.verification(casex,casey,1,1,blanc)==2){
+                    if(this.verification(casex,casey,2,2,blanc)==1){
+                        chemin.add(lesCases[casex+2][casey+2]);
+                    }
                 }
+
+
+
+                if(this.verification(casex,casey,1,-1,blanc)==1){
+                    chemin.add(lesCases[casex+1][casey-1]);
+                }else if(this.verification(casex,casey,1,-1,blanc)==2){
+                    if(this.verification(casex,casey,2,-2,blanc)==1){
+                        chemin.add(lesCases[casex+2][casey-2]);
+                    }
+                }
+
+
+                if(this.verification(casex,casey,-1,-1,blanc) == 2){
+                    if(this.verification(casex,casey,-2,-2,blanc) ==1){
+                        chemin.add(lesCases[casex-2][casey-2]);
+                    }
+                }
+
+                if(this.verification(casex,casey,-1,1,blanc)==2){
+                    if(this.verification(casex,casey,-2,2,blanc) == 1){
+                        chemin.add(lesCases[casex-2][casey+2]);
+                    }
+                }
+            }else{
+                while(this.verification(casex,casey,-1,-1,blanc) == 1){
+                    chemin.add(lesCases[casex-1][casey-1]);
+                    casex++;
+                    casey++;
+                }
+                while(this.verification(casex,casey,-1,1,blanc) == 1){
+                    chemin.add(lesCases[casex-1][casey+1]);
+                    casex++;
+                    casey++;
+                }
+
             }
 
 
 
-            if(this.verification(casex,casey,1,-1,blanc)==1){
-                chemin.add(lesCases[casex+1][casey-1]);
-            }else if(this.verification(casex,casey,1,-1,blanc)==2){
-                if(this.verification(casex,casey,2,-2,blanc)==1){
-                    chemin.add(lesCases[casex+2][casey-2]);
-                }
-            }
 
 
-            if(this.verification(casex,casey,-1,-1,blanc) == 2){
-                if(this.verification(casex,casey,-2,-2,blanc) ==1){
-                    chemin.add(lesCases[casex-2][casey-2]);
-                }
-            }
-
-            if(this.verification(casex,casey,-1,1,blanc)==2){
-                if(this.verification(casex,casey,-2,2,blanc) == 1){
-                    chemin.add(lesCases[casex-2][casey+2]);
-                }
-            }
 
         }else{
-
-
-
-
-
-            if(this.verification(casex,casey,-1,-1,blanc) == 1){
-                chemin.add(lesCases[casex-1][casey-1]);
-            }else if(this.verification(casex,casey,-1,-1,blanc) == 2){
-                if(this.verification(casex,casey,-2,-2,blanc) ==1){
-                    chemin.add(lesCases[casex-2][casey-2]);
+            if(pieceCase instanceof Pion){
+                if(this.verification(casex,casey,-1,-1,blanc) == 1){
+                    chemin.add(lesCases[casex-1][casey-1]);
+                }else if(this.verification(casex,casey,-1,-1,blanc) == 2){
+                    if(this.verification(casex,casey,-2,-2,blanc) ==1){
+                        chemin.add(lesCases[casex-2][casey-2]);
+                    }
                 }
-            }
 
 
 
-            if(this.verification(casex,casey,-1,1,blanc)==1){
-                chemin.add(lesCases[casex-1][casey+1]);
-            }else  if(this.verification(casex,casey,-1,1,blanc)==2){
-                if(this.verification(casex,casey,-2,2,blanc) == 1){
-                    chemin.add(lesCases[casex-2][casey+2]);
+                if(this.verification(casex,casey,-1,1,blanc)==1){
+                    chemin.add(lesCases[casex-1][casey+1]);
+                }else  if(this.verification(casex,casey,-1,1,blanc)==2){
+                    if(this.verification(casex,casey,-2,2,blanc) == 1){
+                        chemin.add(lesCases[casex-2][casey+2]);
+                    }
                 }
-            }
 
 
-            if(this.verification(casex,casey,1,-1,blanc)==2){
-                if(this.verification(casex,casey,2,-2,blanc)==1){
-                    chemin.add(lesCases[casex+2][casey-2]);
+                if(this.verification(casex,casey,1,-1,blanc)==2){
+                    if(this.verification(casex,casey,2,-2,blanc)==1){
+                        chemin.add(lesCases[casex+2][casey-2]);
+                    }
                 }
-            }
 
 
-            if(this.verification(casex,casey,1,1,blanc)==2){
-                if(this.verification(casex,casey,2,2,blanc)==1){
-                    chemin.add(lesCases[casex+2][casey+2]);
+                if(this.verification(casex,casey,1,1,blanc)==2){
+                    if(this.verification(casex,casey,2,2,blanc)==1){
+                        chemin.add(lesCases[casex+2][casey+2]);
+                    }
                 }
+            }else{
+
             }
 
 
